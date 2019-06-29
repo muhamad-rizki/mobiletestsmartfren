11397c2e700ae233114b099c989697f23bf7b2c4
Muhamad Rizki <muhrizkie88@gmail.com>
2019-06-29 16:14:16 +0700

    add tmdb config to app store

--- src/modules/AppState.js
+++ src/modules/AppState.js
@@ -1,6 +1,7 @@
 // @flow
 type AppStateType = {
   isFirstOpen: boolean,
+  internetStatus: number,
 };
 
 type ActionType = {
@@ -10,13 +11,30 @@ type ActionType = {
 
 export const initialState: AppStateType = {
   isFirstOpen: true,
+  internetStatus: 200,
 };
 
 export const SET_FIRST_OPEN = 'AppState/SET_FIRST_OPEN';
+export const ACTION_SET_TMDB_CONFIG = 'AppState/ACTION_SET_TMDB_CONFIG';
+export const ACTION_SET_INET_STATUS = 'AppState/ACTION_SET_INET_STATUS';
 
 export function setAppOpened(): ActionType {
   return {
     type: SET_FIRST_OPEN,
+  };
+}
+
+export function setTMDBConfig(payload): ActionType {
+  return {
+    type: ACTION_SET_TMDB_CONFIG,
+    payload,
+  };
+}
+
+export function setInternetStatus(payload): ActionType {
+  return {
+    type: ACTION_SET_INET_STATUS,
+    payload,
   };
 }
 
@@ -29,6 +47,16 @@ export default function AppStateReducer(
       return {
         ...state,
         isFirstOpen: false,
+      };
+    case ACTION_SET_TMDB_CONFIG:
+      return {
+        ...state,
+        ...action.payload,
+      };
+    case ACTION_SET_INET_STATUS:
+      return {
+        ...state,
+        internetStatus: action.payload,
       };
     default:
       return state;
