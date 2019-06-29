d333bbdfb6a2f05544664b15398e6871d5ef0291
Muhamad Rizki <muhrizkie88@gmail.com>
2019-06-29 12:53:32 +0700

    add rn renimated

--- android/app/src/main/java/id/layarkaca21/MainApplication.java
+++ android/app/src/main/java/id/layarkaca21/MainApplication.java
@@ -3,6 +3,7 @@ package id.layarkaca21;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.reanimated.ReanimatedPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.cmcewen.blurview.BlurViewPackage;
 import org.reactnative.camera.RNCameraPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReanimatedPackage(),
             new RNGestureHandlerPackage(),
             new BlurViewPackage(),
             new RNCameraPackage(),
