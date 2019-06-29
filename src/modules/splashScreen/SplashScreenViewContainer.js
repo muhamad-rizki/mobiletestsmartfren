0b0fd88dc620043f926ca232d601d589a28fc749
Muhamad Rizki <muhrizkie88@gmail.com>
2019-06-29 16:14:49 +0700

    load tmdb config to local redux store

--- src/modules/splashScreen/SplashScreenViewContainer.js
+++ src/modules/splashScreen/SplashScreenViewContainer.js
@@ -1,9 +1,12 @@
 // @flow
+import { AxiosError } from 'axios';
 import { connect } from 'react-redux';
 import { BackHandler } from 'react-native';
 import { compose, lifecycle } from 'recompose';
 import SplashScreenView from './SplashScreenView';
 import { ResetNavigator } from '../../env';
+import InvokeHelper from '../../components/InvokeHelper';
+import { setTMDBConfig, setInternetStatus } from '../AppState';
 
 
 export default compose(
@@ -13,14 +16,25 @@ export default compose(
     }),
     (dispatch, { navigation }) => ({
       nav: navigation,
+      setConfig: config => dispatch(setTMDBConfig(config)),
+      setInetStatus: status => dispatch(setInternetStatus(status)),
     }),
   ),
   lifecycle({
+    componentWillMount() {
+      const { setConfig, setInetStatus } = this.props;
+      new InvokeHelper().getConfiguration()
+        .then((response) => {
+          setConfig(response.data);
+          setInetStatus(200);
+        })
+        .catch((error: AxiosError) => setInetStatus(error.code));
+    },
     componentDidMount() {
       const { navigation } = this.props;
       setTimeout(() => {
         ResetNavigator(navigation, 'HomeScreen');
-      }, 3500);
+      }, 3000);
     },
   }),
 )(SplashScreenView);
