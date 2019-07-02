d651140a025a73718e52472b2cbf2e9d99694c9b
Muhamad Rizki <muhrizkie88@gmail.com>
2019-07-02 11:04:56 +0700

    update test case and snapshot for rendering at least with one item

--- src/components/__tests__/PopularList.spec.js
+++ src/components/__tests__/PopularList.spec.js
@@ -25,4 +25,45 @@ describe('PopularList Component', () => {
     );
     expect(wrapper).toMatchSnapshot();
   });
+  it('renders as expected with at least one item', () => {
+    const setPopLoading = jest.fn();
+    const wrapper = shallow(
+      <PopularList
+        popular={{
+          results: [
+            {
+              vote_count: 1950,
+              id: 420817,
+              video: false,
+              vote_average: 7.2,
+              title: 'Aladdin',
+              popularity: 128.906,
+              poster_path: '/3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg',
+              original_language: 'en',
+              original_title: 'Aladdin',
+              genre_ids: [12],
+              backdrop_path: '/v4yVTbbl8dE1UP2dWu5CLyaXOku.jpg',
+              adult: false,
+              overview: 'A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \'fight clubs\' forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.',
+              release_date: '2019-05-22',
+            }
+          ],
+        }}
+        imgUrl="https://image.tmdb.org/t/p/"
+        popularIndex={0}
+        setPopLoading={setPopLoading}
+        loadingPopular={false}
+        activeTabIndex={1}
+        // eslint-disable-next-line jsx-a11y/tabindex-no-positive
+        tabIndex={1}
+        genres={[
+          {
+            id: 12,
+            name: 'Drama'
+          }
+        ]}
+      />,
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
 });
